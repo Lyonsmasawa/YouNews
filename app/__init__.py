7f0ddd22d44f56f2f39c367292abd5094de6a674
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-02-28 01:38:12 +0300

    update structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
-from .config import DevConfig
+from config import config_options
 from flask_bootstrap import Bootstrap
 
-app = Flask(__name__, instance_relative_config = True)
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+bootstrap = Bootstrap()
 
-bootstrap = Bootstrap(app) 
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config_options[config_name])
 
-from app import views
-from app import error
+    bootstrap.init_app(app) 
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
--- config.py
+++ config.py
@@ -1,10 +1,12 @@
+import os
+
 class Config:
     '''
     General configuration parent class
     '''
     NEWS_BASE_URL = 'https://newsapi.org/v2/top-headlines/sources?&apikey={}'
     NEWS_ARTICLE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apikey={}'
-    
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
 
 class ProdConfig(Config):
@@ -26,3 +28,8 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+
+config_options = {
+    'development' :DevConfig,
+    'production' :ProdConfig
+}
