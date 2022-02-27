9b7980554961e507f395546312e0f2f042c842e2
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-02-27 12:55:05 +0300

    test class innititalization

--- app/models/news_test.py
+++ app/models/news_test.py
@@ -15,7 +15,18 @@ class NewsTest(unittest.TestCase):
     
     def test_instance(self):
         """Test instance"""
-        self.assertTrue(isinstance(self.new_news, New))
+        self.assertTrue(isinstance(self.new_news, News))
+
+    def test_init(self):
+        """"
+        Test initialization
+        """
+        self.assertEqual(self.new_news.id, 1234)
+        self.assertEqual(self.new_news.name, "News")
+        self.assertEqual(self.new_news.description, "This is news")
+        self.assertEqual(self.new_news.url, "https://newslink.com")
+        self.assertEqual(self.new_news.category, "category")
+        self.assertEqual(self.new_news.language, "En")
 
 if  __name__ == '__main__':
     unittest.main()
