b8ac4457047f2958d0236e4258c5d420e10e7c5a
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-02-27 12:40:31 +0300

    create test class

--- app/models/news_test.py
+++ app/models/news_test.py
@@ -5,4 +5,8 @@ class NewsTest(unittest.TestCase):
     """
     Test class to test behaviour of class
     """
-    
+    def setUp(self):
+        """
+        run before every test
+        """
+        self.new_news = News(1234, "News", "This is news", "https://newslink.com", "category", "En" )
